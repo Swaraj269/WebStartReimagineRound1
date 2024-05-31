41abb634180183a6472b4334ae0072e744da7c2b
Swaraj Singh <swarajsingh494@gmail.com>
2024-06-01 03:05:11 +0530

    made menu and second page animation

--- script.js
+++ script.js
@@ -37,6 +37,15 @@ var nav3 = document.querySelector('#navpart3');
 var loadertext = document.querySelector('#loadingtext');
 var loadercounter = document.querySelector('#loadercounter');
 var count = 0;
+var producttag = document.querySelector('#producttag');
+var products = document.querySelector('#products');
+var shopall = document.querySelector('#shop');
+var protein = document.querySelector('#protein');
+var creatine = document.querySelector('#creatine');
+var BCCA = document.querySelector('#BCCA');
+var clothing = document.querySelector('#clothing');
+var accessories = document.querySelector('#accessories');
+var productphoto = document.querySelector('#productphoto img');
 
 
 
@@ -45,25 +54,25 @@ function loadingAndHomeAnimation(){
     var tl2 = gsap.timeline();
 tl2.from("#loadingtext h1",{
     opacity: 0,
-    delay: 0.6,
-    duration: 0.7,
+    delay: 0.3,
+    duration: 0.3,
     y: 100,
     onStart: function() {
         Shery.textAnimate("#loadingtext h1" /* Element to target.*/, {
             //Parameters are optional.
             style: 1,
             y: 90,
-            delay: 0.1,
-            duration: 0.4,
+            delay: 0.06,
+            duration: 0.001,
             ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-            multiplier: 0.5,
+            multiplier: 0.3,
         });
         
-    }
+    },
 })
 tl2.from("#loadingtext h2",{
     opacity: 0,
-    delay: 0.6,
+    delay: 2,
     duration: 0.7,
     y: 100,
     onComplete: function(){
@@ -75,7 +84,7 @@ tl2.from("#loadingtext h2",{
             else{
                 setTimeout(functioncounter);
             }
-        }, 23)
+        }, 20)
     }
 })
 tl2.to("#loader",{
@@ -155,3 +164,219 @@ window.addEventListener('mousemove',function(){
     }, 400);
 })
 
+function productmenudownanimation(){
+    producttag.addEventListener('mouseover',function(){
+        gsap.to("#products",{
+            y:"100%",
+            duration: 0.5,
+            ease: "power4.inOut",
+        })
+    })
+    products.addEventListener('mouseleave',function(){
+        gsap.to("#products",{
+            y:0,
+            duration: 0.5,
+            ease: "power4.inOut",
+        })
+    })
+    
+    shopall.addEventListener('mouseover',function(){
+        gsap.to('#shopall',{
+            display: 'initial',
+            opacity: 1,
+            duration: 0.2,
+            ease: "bounce.inOut",
+        })
+        productphoto.style.display = "initial";
+        productphoto.src = "https://cdn.shopify.com/s/files/1/0690/7723/7977/t/4/assets/d543ec9c521c--shopall-219a3f.jpg?v=1715186587";
+    })
+    shopall.addEventListener('mouseleave',function(){
+        gsap.to('#shopall',{
+            display: 'none',
+            opacity: 0,
+            duration: 0.2,
+            ease: "bounce.inOut",
+        })
+        productphoto.style.display = "none";
+    })
+    
+    protein.addEventListener('mouseover',function(){
+        gsap.to('#shopprotein',{
+            display: 'initial',
+            opacity: 1,
+            duration: 0.2,
+            ease: "bounce.inOut",
+        })
+        productphoto.style.display = "initial";
+        productphoto.src = "https://cdn.shopify.com/s/files/1/0690/7723/7977/t/4/assets/d543ec9c521c--protein-eb5e33.jpg?v=1715186349";
+    })
+    protein.addEventListener('mouseleave',function(){
+        gsap.to('#shopprotein',{
+            display: 'none',
+            opacity: 0,
+            duration: 0.2,
+            ease: "bounce.inOut",
+        })
+        productphoto.style.display = "none";
+    })
+    
+    creatine.addEventListener('mouseover',function(){
+        gsap.to('#shopcreatine',{
+            display: 'initial',
+            opacity: 1,
+            duration: 0.2,
+            ease: "bounce.inOut",
+        })
+        productphoto.style.display = "initial";
+        productphoto.src = "https://cdn.shopify.com/s/files/1/0690/7723/7977/t/4/assets/d543ec9c521c--creatine-e7ea9c.jpg?v=1715186354";
+    })
+    creatine.addEventListener('mouseleave',function(){
+        gsap.to('#shopcreatine',{
+            display: 'none',
+            opacity: 0,
+            duration: 0.2,
+            ease: "bounce.inOut",
+        })
+        productphoto.style.display = "none";
+    })
+    
+    BCCA.addEventListener('mouseover',function(){
+        gsap.to('#shopBCCA',{
+            display: 'initial',
+            opacity: 1,
+            duration: 0.2,
+            ease: "bounce.inOut",
+        })
+        productphoto.style.display = "initial";
+        productphoto.src = "https://cdn.shopify.com/s/files/1/0690/7723/7977/t/4/assets/d543ec9c521c--bcaa-c241d8.jpg?v=1715186359";
+    })
+    BCCA.addEventListener('mouseleave',function(){
+        gsap.to('#shopBCCA',{
+            display: 'none',
+            opacity: 0,
+            duration: 0.2,
+            ease: "bounce.inOut",
+        })
+        productphoto.style.display = "none";
+    })
+    
+    clothing.addEventListener('mouseover',function(){
+        gsap.to('#shopclothing',{
+            display: 'initial',
+            opacity: 1,
+            duration: 0.2,
+            ease: "bounce.inOut",
+        })
+        productphoto.style.display = "initial";
+        productphoto.src = "https://cdn.shopify.com/s/files/1/0690/7723/7977/t/4/assets/d543ec9c521c--tshirt-88de72.jpg?v=1715186364";
+    })
+    clothing.addEventListener('mouseleave',function(){
+        gsap.to('#shopclothing',{
+            display: 'none',
+            opacity: 0,
+            duration: 0.2,
+            ease: "bounce.inOut",
+        })
+        productphoto.style.display = "none";
+    })
+    
+    accessories.addEventListener('mouseover',function(){
+        gsap.to('#shopaccessories',{
+            display: 'initial',
+            opacity: 1,
+            duration: 0.2,
+            ease: "bounce.inOut",
+        })
+        productphoto.style.display = "initial";
+        productphoto.src = "https://cdn.shopify.com/s/files/1/0690/7723/7977/t/4/assets/d543ec9c521c--Metal-Shaker-53da01.jpg?v=1715247352";
+    })
+    accessories.addEventListener('mouseleave',function(){
+        gsap.to('#shopaccessories',{
+            display: 'none',
+            opacity: 0,
+            duration: 0.2,
+            ease: "bounce.inOut",
+        })
+        productphoto.style.display = "none";
+    })
+}
+
+productmenudownanimation();
+
+function secondpageanimation(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+    
+    const options = {
+        threshold: 0.7
+    };
+    
+    const observerCallback = (entries) => {
+        entries.forEach(entry => {
+            const show1 = entry.target.querySelector(".show1");
+            const show2 = entry.target.querySelector(".show2");
+            if (entry.isIntersecting) {
+                gsap.to(show1, {
+                    y: 0,
+                    opacity: 1,
+                    scale: 1,
+                    duration: 0.2,
+                });
+                gsap.to(show2, {
+                    opacity: 1,
+                    scale: 1,
+                    duration: 0.4,
+                });
+            } else {
+                gsap.to(show1, {
+                    y: 50,
+                    opacity: 0,
+                    scale: 0,
+                    duration: 0.4
+                });
+                gsap.to(show2, {
+                    opacity: 0,
+                    scale: 0,
+                    duration: 0.3
+                });
+            }
+        });
+    };
+    const observer = new IntersectionObserver(observerCallback, options);
+    const slides = document.querySelectorAll('.swiper-slide');
+    slides.forEach(slide => {
+        observer.observe(slide);
+    });
+var page2 = document.querySelector('#page2');
+    page2.addEventListener('mousemove',function(e){
+        gsap.to('#seccursor',{
+            opacity: 1,
+            x: e.x,
+            y: e.y,
+            duration: 0.15,
+        })
+    })
+    page2.addEventListener('wheel',function(){
+        gsap.to('#seccursor',{
+            opacity: 0,
+        })
+    })
+    page2.addEventListener('mouseleave',function(){
+        gsap.to('#seccursor',{
+            opacity: 0,
+        })
+    })
+}
+
+secondpageanimation();
